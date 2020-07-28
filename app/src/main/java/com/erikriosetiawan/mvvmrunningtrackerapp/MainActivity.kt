c34776cf9f606affbb032267f9514279b9ee0269
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-29 01:07:43 +0700

    Clean the code

--- app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.erikriosetiawan.mvvmrunningtrackerapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiKey = BuildConfig.GOOGLE_MAPS_API_KEY
+        Log.i("GOOGLE_MAPS_API_KEY", apiKey)
     }
 }

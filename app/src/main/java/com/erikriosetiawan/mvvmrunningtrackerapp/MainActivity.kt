c42be1867a6d0639afe7ae83ea98c86c4fc85a45
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-07-29 01:08:34 +0700

    Clean the code

--- app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.erikriosetiawan.mvvmrunningtrackerapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {

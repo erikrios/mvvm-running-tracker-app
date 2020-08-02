f5321044ebe601e3bae06f37c4a4dfa125acfb09
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-02 21:39:05 +0700

    Implements Dagger-Hilt dependency injection

--- app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.erikriosetiawan.mvvmrunningtrackerapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.mvvmrunningtrackerapp.db.RunDAO
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var runDao: RunDAO
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("runDao", "RUNDAO: ${runDao.hashCode()}")
     }
 }

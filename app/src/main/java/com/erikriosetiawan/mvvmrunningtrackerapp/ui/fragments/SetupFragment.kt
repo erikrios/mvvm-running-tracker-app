9ecc209ba5ffe8e04bd059873ab369fa426c7268
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-04 20:16:28 +0700

    Create SetupFragment.kt

---
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/fragments/SetupFragment.kt
@@ -0,0 +1,8 @@
+package com.erikriosetiawan.mvvmrunningtrackerapp.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.erikriosetiawan.mvvmrunningtrackerapp.R
+
+class SetupFragment : Fragment(R.layout.fragment_setup) {
+
+}

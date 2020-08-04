4420ff1b0ebb1b4d4bc9a189489d5df0ee79db35
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-04 20:08:07 +0700

    Create MainRepository.kt

---
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/repositories/MainRepository.kt
@@ -0,0 +1,31 @@
+package com.erikriosetiawan.mvvmrunningtrackerapp.repositories
+
+import com.erikriosetiawan.mvvmrunningtrackerapp.db.Run
+import com.erikriosetiawan.mvvmrunningtrackerapp.db.RunDAO
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    val runDao: RunDAO
+) {
+    suspend fun insertRun(run: Run) = runDao.insertRun(run)
+
+    suspend fun deleteRun(run: Run) = runDao.deleteRun(run)
+
+    fun getAllRunsSortedByDate() = runDao.getAllRunsSortedByDate()
+
+    fun getAllRunsSortedByDistance() = runDao.getAllRunsSortedByDistance()
+
+    fun getAllRunsSortedByTimeInMillis() = runDao.getAllRunsSortedByTimeInMillis()
+
+    fun getAllRunsSortedAvgSpeed() = runDao.getAllRunsSortedByAvgSpeed()
+
+    fun getAllRunsSortedByCaloriesBurned() = runDao.getAllRunsSortedByCaloriesBurned()
+
+    fun getTotalAvgSpeed() = runDao.getTotalAvgSpeed()
+
+    fun getTotalDistance() = runDao.getTotalDistance()
+
+    fun getTotalCaloriesBurned() = runDao.getTotalCaloriesBurned()
+
+    fun getTotalTimeInMillis() = runDao.getTotalTimeInMillis()
+}

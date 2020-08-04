466616005f1b092195dada8668941927be2f8809
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-04 21:48:56 +0700

    Add AndroidEntryPoint

--- app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.erikriosetiawan.mvvmrunningtrackerapp.R
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {

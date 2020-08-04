c246668ca156452057feeae22cbf563c85b8711d
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-04 20:01:43 +0700

    Move MainActivity.kt

--- app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.erikriosetiawan.mvvmrunningtrackerapp
+package com.erikriosetiawan.mvvmrunningtrackerapp.ui
 
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.erikriosetiawan.mvvmrunningtrackerapp.R
 import com.erikriosetiawan.mvvmrunningtrackerapp.db.RunDAO
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject

73d2654a803c3c8c6dede2b03d8a08febff5a527
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-04 20:19:38 +0700

    Add AndroidEntryPoint annotations

--- app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/fragments/RunFragment.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/fragments/RunFragment.kt
@@ -4,7 +4,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.erikriosetiawan.mvvmrunningtrackerapp.R
 import com.erikriosetiawan.mvvmrunningtrackerapp.ui.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RunFragment : Fragment(R.layout.fragment_run) {
 
     private val viewModel: MainViewModel by viewModels()
--- app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/fragments/StatisticsFragment.kt
@@ -4,7 +4,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.erikriosetiawan.mvvmrunningtrackerapp.R
 import com.erikriosetiawan.mvvmrunningtrackerapp.ui.viewmodels.StatisticsViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
     private val viewModel: StatisticsViewModel by viewModels()

2bc355d0701226c22d6722503ef512e102b58680
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-08-04 21:43:26 +0700

    Integrate navigation component with activity

--- app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/erikriosetiawan/mvvmrunningtrackerapp/ui/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.erikriosetiawan.mvvmrunningtrackerapp.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.erikriosetiawan.mvvmrunningtrackerapp.R
-import com.erikriosetiawan.mvvmrunningtrackerapp.db.RunDAO
-import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
+import kotlinx.android.synthetic.main.activity_main.*
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var runDao: RunDAO
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when (destination.id) {
+                    R.id.settingsFragment, R.id.runFragment, R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
